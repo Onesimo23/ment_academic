86109f6bf7e689ee39929c6cd5223d45e09b4e80
José Onésimo Abel Nuvunga <onesimonuvunga@gmail.com>
2024-01-16 19:59:08 +0200

    initial commit (mg and models)

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MentorController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,6 +27,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   
+    Route::get('/mentors/create', [MentorController::class, 'create'])->name('mentors.create');
+    Route::post('/mentors', [MentorController::class, 'store'])->name('mentors.store');
+    Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+
+
 });
 
 require __DIR__.'/auth.php';
